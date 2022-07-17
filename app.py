5372746d418ce6416103caf5aefa1bbe693d363f
Madushan Sandaruwan <madushansandaru1@gmail.com>
2022-07-17 17:09:13 +0530

    up

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import Flask, render_template, request, jsonify, flash
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
-app.secret_key = "GREET_APP_TEST"
+app.secret_key = "manbearpig_MUDMAN888"
 
-
-@app.route("/")
+@app.route("/hello")
 def index():
-    flash("What's your name?")
-    return render_template("index.html")
+	flash("what's your name?")
+	return render_template("index.html")
 
-
-@app.route("/greet", methods=["POST", "GET"])
-def greet():
-    flash("Hi " + str(request.form['name_input']))
-    return render_template("index.html")
+@app.route("/greet", methods=['POST', 'GET'])
+def greeter():
+	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+	return render_template("index.html")
